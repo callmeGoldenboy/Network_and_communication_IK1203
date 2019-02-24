b0b925890f32a33c0539b76abcc133d58345beea
Natan <natanteferi@gmail.com>
2019-02-24 15:24:15 +0100

    Improved version of HTTPAsk that serves multiple clients simultaneously

---
+++ task4/ConcHTTPAsk.java
@@ -0,0 +1,31 @@
+import java.net.*;
+import java.io.*;
+
+public class ConcHTTPAsk {
+  public static void main( String[] args) throws MalformedURLException {
+     int serverPort;
+     try{
+       serverPort = Integer.parseInt(args[0]);
+
+     }catch(Exception ex){
+       System.err.println("Usage: HTTPAsk host port <data to server>");
+       return;
+     }
+
+     try{
+       ServerSocket serversocket = new ServerSocket(serverPort);
+       System.out.println("Listening for connection on port " + serverPort + "...");
+     while(true){
+       Socket cSocket = serversocket.accept();
+       Thread myThread = new Thread(new MyRunnable(cSocket,serverPort));
+       myThread.start();
+
+       }
+
+   }catch(Exception ex){
+         System.out.println(ex);
+       }
+
+
+    }///end of main
+}
